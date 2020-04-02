ca4130561fe42a2141a6e2691997316870fd2594
Matthew Sausner <matthew.sausner@gmail.com>
2020-04-02 19:28:53 -0400

    GIRT album art, basic resize

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express')
 const resize = require('./resize')
 
 const server = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 server.get('/', (req, res) => {
 
-	const widthString = req.query.width
-	const heightString = req.query.height
+	const widthString = 700
+	const heightString = 700
 	const format = req.query.format
 
 	let width, height
@@ -19,7 +19,7 @@ server.get('/', (req, res) => {
 	}
 
     res.type('image/jpg')
-	resize('rtj.jpg', format, width, height).pipe(res)
+	resize('girtv1.jpg', format, width, height).pipe(res)
 })
 
 server.listen(port, () => {
