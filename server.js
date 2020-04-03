8fcdf4b717f4f353b371d60d28660363189f2a8f
Matthew Sausner <matthew.sausner@gmail.com>
2020-04-02 23:13:37 -0400

    fucking shit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const resize = require('./resize')
+
+const server = express()
+const port = process.env.PORT || 3000
+
+server.get('/', (req, res) => {
+
+	const widthString = 450
+	const heightString = 450
+	const format = req.query.format
+
+	let width, height
+	if (widthString) {
+	  width = parseInt(widthString)
+	}
+	if (heightString) {
+	  height = parseInt(heightString)
+	}
+
+    res.type('image/jpg')
+	resize('girtv1.jpg', format, width, height).pipe(res)
+})
+
+server.listen(port, () => {
+	console.log('server started')
+})
