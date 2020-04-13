66c690ee409f85886f40f64e13f76676f6aa0cca
Matthew Sausner <matthew.sausner@gmail.com>
2020-04-13 11:34:07 -0400

    updated sometimes!

--- client/src/components/Example4/Component.js
+++ client/src/components/Example4/Component.js
@@ -66,4 +66,7 @@ export const Example4Container = ({ initialLoadDone, audionState, coverDeg, toDe
   <div className="message-bottom" vertical-align="center">
     <div >moss don't</div>
   </div>
+  <div className="message-bottomer" vertical-align="center">
+    <div >updated sometimes!</div>
+  </div>
 </>;
