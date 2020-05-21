56beec38c3f47e33f7c400fe7e6440eeebf7701e
Matthew Sausner <matthew.sausner@gmail.com>
2020-05-21 17:01:12 -0400

    underline

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb+srv://saus:jeriko69@cluster0-bao5y.mongodb.net/test?retryWrites=true&w=majority&readPreference=secondary"
+  mongoURI: "mongodb+srv://saus:jeriko69@cluster0-bao5y.mongodb.net/test?retryWrites=true&w=majority&readPreference=secondary&authSource=admin"
 }
